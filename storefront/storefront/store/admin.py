4c39102855c75d3863e8c3f09edafc1be900f85d
tammytdo11 <tammytdo11@gmail.com>
2023-03-14 00:26:14 -0700

    registered models to admin file

--- storefront/storefront/store/admin.py
+++ storefront/storefront/store/admin.py
@@ -1,3 +1,48 @@
+from django.db.models import Count
 from django.contrib import admin
+from django.http import HttpRequest
+from . import models
 
 # Register your models here.
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin):
+  list_display = ['title', 'unit_price', 'inventory_status', 'collection_title']
+  list_editable = ['unit_price']
+  list_per_page = 10
+  list_select_related = ['collection']
+
+  @admin.display(ordering='inventory')
+  def inventory_status(self, product):
+    if product.inventory < 10:
+      return "Low"
+    return "OK"
+  
+  def collection_title(self, product):
+    return product.collection.title
+
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+  list_display = ['title', 'products_count']
+
+  @admin.display(ordering='products_count')
+  def products_count(self, collection):
+    return collection.products_count
+  
+  def get_queryset(self, request):
+    return super().get_queryset(request).annotate(
+      products_count = Count('product')
+    )
+
+@admin.register(models.Customer)
+class CustomersAdmin(admin.ModelAdmin):
+  list_display = ['first_name', 'last_name', 'membership']
+  list_editable = ['membership']
+  ordering = ['first_name', 'last_name']
+  list_per_page = 10
+
+
+@admin.register(models.Order)
+class OrdersAdmin(admin.ModelAdmin):
+  list_display = ['id', 'placed_at', 'customer']
+
--- storefront/storefront/store/models.py
+++ storefront/storefront/store/models.py
@@ -10,7 +10,12 @@ class Collection(models.Model):
     title = models.CharField(max_length=255)
     featured_product = models.ForeignKey(
         'Product', on_delete=models.SET_NULL, null=True, related_name='+')
-
+    
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
@@ -20,8 +25,12 @@ class Product(models.Model):
     last_update = models.DateTimeField(auto_now=True)
     collection = models.ForeignKey(Collection, on_delete=models.PROTECT)
     slug = models.SlugField(default="-")
-    # promotions = models.ManyToManyField(Promotion)
 
+    def __str__(self) -> str:
+        return self.title
+    
+    class Meta:
+        ordering = ['title']
 
 class Customer(models.Model):
     MEMBERSHIP_BRONZE = 'B'
@@ -41,6 +50,11 @@ class Customer(models.Model):
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
 
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+    
+    class Meta:
+        ordering = ['first_name', 'last_name']
 
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
